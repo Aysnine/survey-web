b2c8ebf9c0afe561cfb2291fde4704fba414de55
CNine <cnine229@gmail.com>
2018-12-19 22:10:44 +0800

    添加全局 plugin, 内置 cookie、log

--- src/pages/survey/main.js
+++ src/pages/survey/main.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
-
+import App from './App.vue'
 import store from './store'
 
-import ElementUI from 'element-ui'
+if (process.env.VUE_APP_PREVIEW == 'on') {
+  require('@/mock/register')
+}
+
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.styl'
 
-import App from './App.vue'
+import '@/plugin'
 
-Vue.use(ElementUI)
+import ElementUI from 'element-ui'
+Vue.use(ElementUI, { size: 'small', locale })
 
 Vue.config.productionTip = false
+Vue.prototype.$env__is_preview = process.env.VUE_APP_PREVIEW === 'on'
 
 new Vue({
   store,
